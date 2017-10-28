10c61d4a328729fea287e319e001083cb4f5b11b
Daniel Mack <zonque@gmail.com>
2017-10-28 12:48:16 +0200

    media/capture: Add support for V4L2 MPLANE devices
    The Linux V4L2 API knows different types of video devices. Single planar
    ones are the most common and are used by all USB cameras, while multiplanar
    types are used by the drivers of some embedded chipsets such as Qualcomm
    Snapdragon.
    
    Support for multiplanar cameras was once introduced to the code base
    (Issue 441836) but then removed again, most probably accidentially (Issue
    585519).
    
    This patch brings back support for these types of devices but in a simpler
    way of implementation than the original approach, as all the device
    specific code is kept internal to the V4L2CaptureDelegate class.
    
    Some static inline functions were turned into class members in order to
    keep the number of parameters low.

--- media/capture/video/linux/video_capture_device_factory_linux.cc
+++ media/capture/video/linux/video_capture_device_factory_linux.cc
@@ -65,7 +65,9 @@ bool HasUsableFormats(int fd, uint32_t capabilities) {
   const std::list<uint32_t>& usable_fourccs =
       VideoCaptureDeviceLinux::GetListOfUsableFourCCs(false);
   v4l2_fmtdesc fmtdesc = {};
-  fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  fmtdesc.type = (capabilities & V4L2_CAP_VIDEO_CAPTURE_MPLANE)
+                     ? V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE
+                     : V4L2_BUF_TYPE_VIDEO_CAPTURE;
   for (; HANDLE_EINTR(ioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc)) == 0;
        ++fmtdesc.index) {
     if (std::find(usable_fourccs.begin(), usable_fourccs.end(),
@@ -113,9 +115,12 @@ std::list<float> GetFrameRateList(int fd,
 
 void GetSupportedFormatsForV4L2BufferType(
     int fd,
+    VideoCaptureApi api,
     VideoCaptureFormats* supported_formats) {
   v4l2_fmtdesc v4l2_format = {};
-  v4l2_format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  v4l2_format.type = (api == VideoCaptureApi::LINUX_V4L2_MULTI_PLANE)
+                         ? V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE
+                         : V4L2_BUF_TYPE_VIDEO_CAPTURE;
   for (; HANDLE_EINTR(ioctl(fd, VIDIOC_ENUM_FMT, &v4l2_format)) == 0;
        ++v4l2_format.index) {
     VideoCaptureFormat supported_format;
@@ -210,21 +215,24 @@ void VideoCaptureDeviceFactoryLinux::GetDeviceDescriptors(
     // http://crbug.com/139356.
     v4l2_capability cap;
     if ((HANDLE_EINTR(ioctl(fd.get(), VIDIOC_QUERYCAP, &cap)) == 0) &&
-        (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE &&
-         !(cap.capabilities & V4L2_CAP_VIDEO_OUTPUT)) &&
+        (cap.capabilities &
+         (V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_VIDEO_CAPTURE_MPLANE)) &&
+        !(cap.capabilities &
+          (V4L2_CAP_VIDEO_OUTPUT | V4L2_CAP_VIDEO_OUTPUT_MPLANE)) &&
         HasUsableFormats(fd.get(), cap.capabilities)) {
       const std::string model_id = GetDeviceModelId(unique_id);
+      VideoCaptureApi api = (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE_MPLANE)
+                                ? VideoCaptureApi::LINUX_V4L2_MULTI_PLANE
+                                : VideoCaptureApi::LINUX_V4L2_SINGLE_PLANE;
 #if defined(OS_CHROMEOS)
       static CameraConfigChromeOS* config = new CameraConfigChromeOS();
       device_descriptors->emplace_back(
-          reinterpret_cast<char*>(cap.card), unique_id, model_id,
-          VideoCaptureApi::LINUX_V4L2_SINGLE_PLANE,
+          reinterpret_cast<char*>(cap.card), unique_id, model_id, api,
           VideoCaptureTransportType::OTHER_TRANSPORT,
           config->GetCameraFacing(unique_id, model_id));
 #else
       device_descriptors->emplace_back(
-          reinterpret_cast<char*>(cap.card), unique_id, model_id,
-          VideoCaptureApi::LINUX_V4L2_SINGLE_PLANE);
+          reinterpret_cast<char*>(cap.card), unique_id, model_id, api);
 #endif
     }
   }
@@ -246,7 +254,8 @@ void VideoCaptureDeviceFactoryLinux::GetSupportedFormats(
   supported_formats->clear();
 
   DCHECK_NE(device.capture_api, VideoCaptureApi::UNKNOWN);
-  GetSupportedFormatsForV4L2BufferType(fd.get(), supported_formats);
+  GetSupportedFormatsForV4L2BufferType(fd.get(), device.capture_api,
+                                       supported_formats);
 }
 
 std::string VideoCaptureDeviceFactoryLinux::GetDeviceModelId(
